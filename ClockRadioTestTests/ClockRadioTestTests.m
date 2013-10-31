b35b5e21448ef4b5ef0e2b07ec3395dc01988e13
rlgoldberg2 <Richard@Richards-MacBook-Air-2.local>
2013-10-31 19:26:30 -0400

    Initial Commit

---
+++ ClockRadioTestTests/ClockRadioTestTests.m
@@ -0,0 +1,34 @@
+//
+//  ClockRadioTestTests.m
+//  ClockRadioTestTests
+//
+//  Created by Richard Goldberg on 10/31/13.
+//  Copyright (c) 2013 Richard Goldberg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClockRadioTestTests : XCTestCase
+
+@end
+
+@implementation ClockRadioTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
